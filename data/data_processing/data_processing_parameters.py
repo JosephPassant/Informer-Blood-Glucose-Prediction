dadb316c4f19ec9b80cffc6b2357a7e8c559f3f6
Joseph Passant <j.passant@liverpool.ac.uk>
2025-05-11 14:52:52 +0100

    updates for readability

--- data/data_processing/data_processing_parameters.py
+++ data/data_processing/data_processing_parameters.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-
-
 base_date = pd.to_datetime('2000-01-01')
 
 slice_size = 96
@@ -15,4 +13,3 @@ start_token_size = 12
 encoder_input_size = 72
 
 decoder_input_size = start_token_size + target_size
-
